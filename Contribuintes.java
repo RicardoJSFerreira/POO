e76ca60baa69d49085ca2eddc9d17e52fbf75caf
ricardomilhazes <36643024+ricardomilhazes@users.noreply.github.com>
2018-05-23 17:20:57 +0100

    Not your TOY

--- Contribuintes.java
+++ Contribuintes.java
@@ -13,7 +13,7 @@ public abstract class Contribuintes
     protected String email;
     protected String nome;
     protected String morada;
-    protected int password;
+    protected String password;
     
     protected ArrayList<Factura> faturas;
     /**
@@ -36,19 +36,8 @@ public abstract class Contribuintes
         return this.morada;
     }
     
-    public int getPassword(){
+    public String getPassword(){
         return this.password;
-    }
-    public boolean equals (Contribuintes con){
-        return (this.NIF==con.getNIF() && this.email==con.getEmail() && this.nome==con.getNome () 
-        && this.morada ==con.getMorada() && this.password==con.getPassword());
-        
-    }
-    
-    public String toString(){
-        return ("Número de contribuinte :"+this.NIF + "Email:" + this.email+ "Nome:"+this.nome+ 
-        "Morada fiscal :"+ this.morada + "Password :" + this.password);
-        
     }
     
     public ArrayList<Factura> getFaturas(){
